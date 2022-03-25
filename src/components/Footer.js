2a513b71ece78082f6b2e8c31895424c26fb2d81
Milvio LuA <milvio@csfarmsolutions.com.au>
2022-03-25 12:38:48 +0700

    digital directory app
    digital directory app

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+function Footer(props) {
+    return ( 
+        <div class="container">
+        <p class="float-end mb-1">
+          <a href="#">Back to top</a>
+        </p>
+        <p class="mb-1">Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+        <p class="mb-0">New to Bootstrap? <a href="/">Visit the homepage</a> or read our <a href="../getting-started/introduction/">getting started guide</a>.</p>
+      </div>
+     );
+}
+
+export default Footer;
